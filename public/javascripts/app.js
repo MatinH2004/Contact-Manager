b88b8f88104490fd08ffb3370f5b6b0d567a17e8
Matin Hassan Pour <matinhp04@gmail.com>
2024-11-29 13:38:44 -0800

    codebase refactor - using app obj

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,106 +1,146 @@
-document.addEventListener('DOMContentLoaded', () => {
-  function registerPartials() {
-    Handlebars.registerPartial('nav_partial', document.querySelector('#nav_partial').innerHTML);
-    Handlebars.registerPartial('tag_partial', document.querySelector('#tag_partial').innerHTML);
-    Handlebars.registerPartial('contact_partial', document.querySelector('#contact_partial').innerHTML);
-    Handlebars.registerPartial('no_contact_partial', document.querySelector('#no_contact_partial').innerHTML);
-    Handlebars.registerPartial('contact_tags_partial', document.querySelector('#contact_tags_partial').innerHTML);
-  }
+const App = {
+  init() {
+    this.templates = this.compileTemplates();
 
-  function compileTemplates() {
+    this.registerPartials();
+    this.renderHomePage()
+    this.bindEvents();
+  },
+
+  compileTemplates() {
     return [...document.querySelectorAll('script[id$="template"]')]
       .reduce((obj, template) => {
         obj[template.id] = Handlebars.compile(template.innerHTML);;
         return obj;
       }, {});
-  }
+  },
 
-  function bindEvents() {
-    const main = document.querySelector('main');
-
-    main.addEventListener('change', fetchContactsByTag);
-    main.addEventListener('focusout', handleInvalidFields);
-    main.addEventListener('input', e => {
-      if (e.target.matches('#search')) fetchContactsBySearch();
+  registerPartials() {
+    document.querySelectorAll('script[id$="partial"]').forEach(partial => {
+      Handlebars.registerPartial(partial.id, partial.innerHTML);
     });
-    main.addEventListener('click', e => {
+  },
+
+  bindEvents() {
+    const clickHandlers = {
+      '.add_contact': () => this.renderForm(),
+      '.cancel': () => this.renderHomePage(),
+      '.submit': () => this.submitForm(),
+      '.edit': target => this.editContact(target),
+      '.delete': target => this.deleteContact(target),
+    };
+
+    document.addEventListener('focusout', this.handleInvalidFields);
+
+    document.addEventListener('change', e => {
+      if (e.target.matches('#tags_list')) this.fetchContactsByTag(e);
+    });
+
+    document.addEventListener('input', e => {
+      if (e.target.matches('#search')) this.fetchContactsBySearch();
+    });
+
+    document.addEventListener('click', e => {
       const target = e.target;
 
-      if (target.matches('.add_contact')) {;
-        renderForm()
-      } else if (target.matches('.cancel')) {
-        renderHomePage();
-      } else if (target.matches('.submit')) {
-        submitForm();
-      } else if (target.matches('.edit')) {
-        editContact(target);
-      } else if (target.matches('.delete')) {
-        deleteContact(target);
+      for (const selector in clickHandlers) {
+        if (target.matches(selector)) {
+          clickHandlers[selector](target);
+          break;
+        }
       }
     });
-  }
+  },
 
-  function handleInvalidFields(e) {
-    const input = e.target;
+  handleInvalidFields(event) {
+    const input = event.target;
     const errorMsg = input.parentElement.querySelector('.error_message');
 
     if (document.querySelector('form') === null || !errorMsg) return;
 
-    if (input.validity.patternMismatch) {
-      input.classList.add('error');
-      errorMsg.classList.add('error');
-    } else if (input.validity.valueMissing) {
-      input.classList.add('error');
-      errorMsg.classList.add('error');
-    } else {
-      input.classList.remove('error');
-      errorMsg.classList.remove('error');
-    }
-  }
+    const hasError = input.validity.patternMismatch || input.validity.valueMissing;
 
-  function containsFormErrors() {
+    input.classList.toggle('error', hasError);
+    errorMsg.classList.toggle('error', hasError);
+  },
+
+  containsFormErrors() {
     document.querySelectorAll('input[type="text"]').forEach(input => {
-      handleInvalidFields({ target: input });
+      this.handleInvalidFields({ target: input });
     });
 
     if (document.querySelectorAll('.error').length !== 0) {
       alert('Please fix form errors before submitting!');
       return true;
     }
-  }
 
-  function getAllTags() {
+    return false;
+  },
+
+  getAllTags() {
     return [...document.querySelectorAll('input[type="checkbox"]:checked')].map(input => {
       return input.closest('label').textContent;
     }).concat(document.querySelector('#new_tag').value)
       .filter(tag => tag && tag.trim().length > 0)
       .join(',');
-  }
+  },
 
-  async function editContact(target) {
-    const updateSubmitBtn = () => {
-      const submitBtn = document.querySelector('.submit');
-      submitBtn.classList.remove('submit');
+  submitForm(modifyContact = false, id = null) {
+    if (this.containsFormErrors()) return;
 
-      submitBtn.addEventListener('click', e => {
-        e.preventDefault();
-        submitForm(true, id);
-      });
+    const data = new FormData(document.querySelector('form'));
+    const json = JSON.stringify({
+      full_name: data.get('name'),
+      email: data.get('email'),
+      phone_number: data.get('telephone'),
+      tags: this.getAllTags(),
+    });
+
+    modifyContact ? this.updateContact(json, id) : this.createContact(json);
+  },
+
+  async createContact(json) {
+    await fetch('http://localhost:3000/api/contacts/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: json,
+    }).then(async () => await this.renderHomePage());
+  },
+
+  async updateContact(json, id) {
+    await fetch(`http://localhost:3000/api/contacts/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: json,
+    }).then(async () => await this.renderHomePage());
+  },
+
+  async deleteContact(target) {
+    const id = target.closest('div').getAttribute('data-id');
+
+    if (confirm('Are you sure you want to delete this contact?')) {
+      try {
+        const response = await fetch(`http://localhost:3000/api/contacts/${id}`, { 
+          method: 'DELETE',
+        });
+
+        if (!response.ok) throw new Error('Failed to delete contact.');
+
+        await this.renderHomePage();
+      } catch (error) {
+        console.error(error);
+      }
     }
+  },
 
-    if (!target.closest('div.contact')) {
-      console.error('Contact Element Not Found');
-      return;
-    }
-    
-    const id = target.closest('div.contact').getAttribute('data-id');
+  async editContact(target) {
+    const getContactId = () => target.closest('div.contact')?.getAttribute('data-id');
 
-    try {
-      const response = await fetch(`http://localhost:3000/api/contacts/${id}`);
-      const contact = await response.json();
-
-      renderForm();
-
+    const setFormFields = (contact) => {
       document.querySelector('#form_title p').textContent = 'Edit Contact';
       document.querySelector('input[name="name"]').value = contact.full_name;
       document.querySelector('input[name="email"]').value = contact.email;
@@ -110,63 +150,37 @@ document.addEventListener('DOMContentLoaded', () => {
         const tags = contact.tags.split(',');
 
         document.querySelectorAll('input[type="checkbox"]').forEach(tag => {
-          if (tags.includes(tag.closest('label').textContent)) tag.checked = true;
+          tag.checked = tags.includes(tag.closest('label').textContent);
         });
       }
-
-      updateSubmitBtn();
-    } catch (error) {
-      console.error('Error fetching contact:', error);
-    };
-  }
-
-  function submitForm(modifyContact = false, id = null) {
-    if (containsFormErrors()) return;
-
-    const data = new FormData(document.querySelector('form'));
-    const json = JSON.stringify({
-      full_name: data.get('name'),
-      email: data.get('email'),
-      phone_number: data.get('telephone'),
-      tags: getAllTags(),
-    });
-
-    modifyContact ? updateContact(json, id) : createContact(json);
-  }
-
-  async function createContact(json) {
-    await fetch('http://localhost:3000/api/contacts/', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: json,
-    }).then(() => renderHomePage());
-  }
-
-  async function updateContact(json, id) {
-    await fetch(`http://localhost:3000/api/contacts/${id}`, {
-      method: 'put',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: json,
-    }).then(() => renderHomePage())
-  }
-
-  function deleteContact(target) {
-    const id = target.closest('div').getAttribute('data-id');
-
-    if (confirm('Are you sure you want to delete this contact?')) {
-      fetch(`http://localhost:3000/api/contacts/${id}`, { 
-        method: 'delete',
-      });
-
-      renderHomePage();
     }
-  }
 
-  async function fetchAllContacts() {
+    const updateSubmitButton = () => {
+      const submitBtn = document.querySelector('.submit');
+      submitBtn.classList.remove('submit');
+      submitBtn.addEventListener('click', e => {
+        e.preventDefault();
+        this.submitForm(true, id);
+      });
+    }
+
+    const id = getContactId();
+
+    try {
+      const response = await fetch(`http://localhost:3000/api/contacts/${id}`);
+      if (!response.ok) throw new Error(`Failed to fetch contact with ID: ${id}`);
+
+      const contact = await response.json();
+
+      this.renderForm();
+      setFormFields(contact);
+      updateSubmitButton(id);
+    } catch (error) {
+      console.error(error);
+    }
+  },
+
+  async fetchAllContacts() {
     try {
       const response = await fetch('http://localhost:3000/api/contacts');
       const data = await response.json();
@@ -179,10 +193,10 @@ document.addEventListener('DOMContentLoaded', () => {
     } catch (error) {
       console.error('Error fetching contacts:', error);
     }
-  }
+  },
 
-  async function fetchContactsBySearch() {
-    const contacts = await fetchAllContacts();
+  async fetchContactsBySearch() {
+    const contacts = await this.fetchAllContacts();
 
     let searchValue = document.querySelector('#search').value.toLowerCase();
     let matches = contacts.filter(({ full_name }) => {
@@ -194,13 +208,13 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     document.querySelector('main #contacts_wrapper').innerHTML = 
-      templates.contacts_template({ contacts: matches, searchValue });
-  }
+      this.templates.contacts_template({ contacts: matches, searchValue });
+  },
 
-  async function fetchContactsByTag(e) {
-    const contacts = await fetchAllContacts();
+  async fetchContactsByTag(event) {
+    const contacts = await this.fetchAllContacts();
 
-    const selectElement = e.target;
+    const selectElement = event.target;
     const selectedIndex = selectElement.selectedIndex;
     const selectedTag = selectElement.value;
 
@@ -209,31 +223,32 @@ document.addEventListener('DOMContentLoaded', () => {
     if (selectedIndex === 0) matches = contacts;
 
     document.querySelector('main #contacts_wrapper').innerHTML =
-      templates.contacts_template({ contacts: matches });
-  }
+      this.templates.contacts_template({ contacts: matches });
+  },
 
-  function renderForm() {
+  async renderHomePage() {
+    const contacts = await this.fetchAllContacts();
+    const tags = [...new Set(contacts.flatMap(({ tags }) => tags))];
+
+    this.renderContacts(contacts);
+    this.renderTags(tags);
+  },
+
+  renderForm() {
     const tags = [...document.querySelectorAll('option')]
       .map(option => option.textContent)
       .slice(1);
+
     document.querySelector('main').innerHTML = 
-      templates.new_contact_template({ tags: tags });
-  }
+      this.templates.new_contact_template({ tags: tags });
+  },
 
-  async function renderHomePage() {
-    const contacts = await fetchAllContacts();
-    const tags = [...new Set(contacts.flatMap(({ tags }) => tags))];
-
-    renderContacts(contacts);
-    renderTags(tags);
-  }
-
-  function renderContacts(data) {
+  renderContacts(data) {
     document.querySelector('main').innerHTML = 
-      templates.home_template({ contacts: data });
-  }
+      this.templates.home_template({ contacts: data });
+  },
 
-  function renderTags(data) {
+  renderTags(data) {
     const tagsList = document.querySelector('#tags_list');
 
     data.forEach(tag => {
@@ -242,46 +257,7 @@ document.addEventListener('DOMContentLoaded', () => {
       select.textContent = tag;
       tagsList.appendChild(select);
     });
-  }
-
-  const templates = compileTemplates();
-
-  registerPartials();
-  renderHomePage();
-  bindEvents();
-});
-
-/*
-
-TODOS:
-[x] delete contacts
-[x] edit contacts
-[x] ajax when using search
-- add focus to searchbar
-- filter contact objects every input
-
-[ ] add search by tag functionality
-[ ] optimize fetching all contacts
- 
--- MAJOR REFACTOR:
-
-const App = {
-  init() {
-    this.templates = this.compileTemplates();
-    this.registerPartials();
-    this.renderHomePage();
-    this.bindEvents();
   },
-
-  compileTemplates() {
-  
-  },
-
-  registerPartials() {
-  
-  },
-
-
 }
 
-*/ 
+document.addEventListener('DOMContentLoaded', App.init.bind(App));
